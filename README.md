6e1f27939adb3bd023ab1be769bb970ed3f35e24
Quaexolith <quaexolith@gmail.com>
2022-07-05 19:46:30 -0400

    Add functions with parameters

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Functions in Rust
 
 Use snake_case all lowercase
+
+# Parameters
+Variables part of the function's signature.
+
+Type must be declared in the function's signature.
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,20 @@ fn main() {
     println!("Hello, world!");
 
     another_function();
+
+    yet_another_function(7);
+
+    print_labeled_measurement(5, 'h');
 }
 
 fn another_function() {
     println!("Another function.");
+}
+
+fn yet_another_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
 }
