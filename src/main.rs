379f6d616f3ac3f509edf0f8ff3219d886939d4b
Quaexolith <quaexolith@gmail.com>
2022-07-05 20:00:38 -0400

    Create functions with return values

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,14 @@ fn main() {
     }; // expression
 
     println!("The value of z is: {z}");
+
+    let mut x = five();
+
+    println!("The value of x is: {x}");
+
+    x = plus_one(34);
+
+    println!("The value of x is: {x}");
 }
 
 fn another_function() {
@@ -29,4 +37,12 @@ fn yet_another_function(x: i32) {
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
